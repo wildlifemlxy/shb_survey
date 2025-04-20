82c84905eb13914721369b0df562760845c71cbe
MosesECSS03 <wildlifemlxy@gmail.com>
2025-04-20 13:47:34 +0800

    ok

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -22,12 +22,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'bird-logo.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'bird-logo.png',
             sizes: '512x512',
             type: 'image/png'
           }
