9c3255970c0c10ad6a9288f2171bcc17b14942c8
MosesECSS03 <wildlifemlxy@gmail.com>
2025-04-24 17:01:13 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || '8080';
+const PORT = process.env.PORT || '3001';
 
 // Middleware
 app.use(cors());
