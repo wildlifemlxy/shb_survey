2f0d63c3b06cc3d6fcde2aeec8ef38bd04a63439
MosesECSS03 <207650274+wildlifemlxy@users.noreply.github.com>
2025-06-26 03:43:38 +0800

    update yml

--- backend/node/app.js
+++ backend/node/app.js
@@ -21,6 +21,7 @@ app.set('view engine', 'pug'); // You can change to 'ejs' or others if needed
 
 app.use(logger('dev'));
 app.use(cors({
+  origin: ['http://localhost:3000', 'https://ashy-glacier-0df38a400.6.azurestaticapps.net'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add any other methods you want to support
   allowedHeaders: ['Content-Type', 'Authorization', 'Content-Disposition'], 
   exposedHeaders: ['Content-Disposition'], // Add this line to expose the header
