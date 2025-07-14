2f1a1c1ded415a3fe8ae694ef2d20ad92483c9d3
MosesECSS03 <207650274+wildlifemlxy@users.noreply.github.com>
2025-07-14 12:17:58 +0800

    Update tokenEncryption.js

--- backend/node/middleware/tokenEncryption.js
+++ backend/node/middleware/tokenEncryption.js
@@ -74,20 +74,18 @@ class TokenEncryptionMiddleware {
       exp: Math.floor(Date.now() / 1000) + (5 * 60) // 5 minutes
     };
 
-    console.log('G1234456:', process.env.JWT_SECRET);
-
-    /*return jwt.sign(payload, process.env.JWT_SECRET, {
+    return jwt.sign(payload, process.env.JWT_SECRET, {
       algorithm: 'HS256'
-    });*/
-    return jwt.sign(payload, 'your-super-secret-jwt-key-change-this-in-production-min-32-chars',
-      { algorithm: 'HS256' });
+    });
+    //return jwt.sign(payload, 'your-super-secret-jwt-key-change-this-in-production-min-32-chars',
+    //  { algorithm: 'HS256' });
   }
 
   // Verify JWT token
   verifyToken(token) {
     try {
-      //return jwt.verify(token, process.env.JWT_SECRET);
-      return jwt.verify(token, 'your-super-secret-jwt-key-change-this-in-production-min-32-chars');
+      return jwt.verify(token, process.env.JWT_SECRET);
+      //return jwt.verify(token, 'your-super-secret-jwt-key-change-this-in-production-min-32-chars');
     } catch (error) {
       throw new Error('Invalid or expired token');
     }
