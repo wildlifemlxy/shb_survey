75e7985302609355472f088219d1e5e03d086edb
MosesECSS03 <207650274+wildlifemlxy@users.noreply.github.com>
2025-08-10 22:16:41 +0800

    Update databaseConnectivity.js

--- backend/node/Database/databaseConnectivity.js
+++ backend/node/Database/databaseConnectivity.js
@@ -25,69 +25,104 @@ class DatabaseConnectivity {
     }
 
   async getAllDocuments(databaseName, collectionName) {
-    const db = this.client.db(databaseName);
-    const collection = db.collection(collectionName);
-    const documents = await collection.find({}).toArray();
-    
-    // Convert ObjectId to string for all documents
-    return documents.map(doc => ({
-      ...doc,
-      _id: doc._id.toString()
-    }));
+    try {
+      const db = this.client.db(databaseName);
+      const collection = db.collection(collectionName);
+      const documents = await collection.find({}).toArray();
+      
+      // Convert ObjectId to string for all documents
+      return documents.map(doc => ({
+        ...doc,
+        _id: doc._id.toString()
+      }));
+    } catch (error) {
+      console.error("Error getting all documents:", error);
+      throw error;
+    } finally {
+      await this.close();
+    }
   }
 
   async insertDocument(databaseName, collectionName, document) {
-    const db = this.client.db(databaseName);
-    const collection = db.collection(collectionName);
-    const result = await collection.insertOne(document);
-    
-    // Return the inserted document with string ID
-    if (result.insertedId) {
-      return {
-        ...result,
-        insertedId: result.insertedId.toString()
-      };
+    try {
+      const db = this.client.db(databaseName);
+      const collection = db.collection(collectionName);
+      const result = await collection.insertOne(document);
+      
+      // Return the inserted document with string ID
+      if (result.insertedId) {
+        return {
+          ...result,
+          insertedId: result.insertedId.toString()
+        };
+      }
+      return result;
+    } catch (error) {
+      console.error("Error inserting document:", error);
+      throw error;
+    } finally {
+      await this.close();
     }
-    return result;
   }
 
   async insertDocuments(databaseName, collectionName, documents) {
-    const db = this.client.db(databaseName);
-    const collection = db.collection(collectionName);
-    const result = await collection.insertMany(documents);
-    
-    // Convert inserted IDs to strings
-    if (result.insertedIds) {
-      const insertedIds = {};
-      Object.keys(result.insertedIds).forEach(key => {
-        insertedIds[key] = result.insertedIds[key].toString();
-      });
-      return {
-        ...result,
-        insertedIds
-      };
+    try {
+      const db = this.client.db(databaseName);
+      const collection = db.collection(collectionName);
+      const result = await collection.insertMany(documents);
+      
+      // Convert inserted IDs to strings
+      if (result.insertedIds) {
+        const insertedIds = {};
+        Object.keys(result.insertedIds).forEach(key => {
+          insertedIds[key] = result.insertedIds[key].toString();
+        });
+        return {
+          ...result,
+          insertedIds
+        };
+      }
+      return result;
+    } catch (error) {
+      console.error("Error inserting documents:", error);
+      throw error;
+    } finally {
+      await this.close();
     }
-    return result;
   }
 
   async updateDocument(databaseName, collectionName, filter, update) {
-    const db = this.client.db(databaseName);
-    const collection = db.collection(collectionName);
-    // Convert string _id to ObjectId if present
-    if (filter._id && typeof filter._id === 'string') {
-      filter._id = new ObjectId(filter._id);
+    try {
+      const db = this.client.db(databaseName);
+      const collection = db.collection(collectionName);
+      // Convert string _id to ObjectId if present
+      if (filter._id && typeof filter._id === 'string') {
+        filter._id = new ObjectId(filter._id);
+      }
+      return await collection.updateOne(filter, update);
+    } catch (error) {
+      console.error("Error updating document:", error);
+      throw error;
+    } finally {
+      await this.close();
     }
-    return await collection.updateOne(filter, update);
   }
 
   async deleteDocument(databaseName, collectionName, filter) {
-    const db = this.client.db(databaseName);
-    const collection = db.collection(collectionName);
-    // Convert string _id to ObjectId if present
-    if (filter._id && typeof filter._id === 'string') {
-      filter._id = new ObjectId(filter._id);
+    try {
+      const db = this.client.db(databaseName);
+      const collection = db.collection(collectionName);
+      // Convert string _id to ObjectId if present
+      if (filter._id && typeof filter._id === 'string') {
+        filter._id = new ObjectId(filter._id);
+      }
+      return await collection.deleteOne(filter);
+    } catch (error) {
+      console.error("Error deleting document:", error);
+      throw error;
+    } finally {
+      await this.close();
     }
-    return await collection.deleteOne(filter);
   }
 
   async getDocument(databaseName, collectionName, email, password) {
@@ -111,6 +146,8 @@ class DatabaseConnectivity {
     } catch (error) {
       console.error("Error retrieving document:", error);
       throw error;
+    } finally {
+      await this.close();
     }
   }
 
@@ -132,6 +169,8 @@ class DatabaseConnectivity {
     } catch (error) {
       console.error("Error finding document:", error);
       throw error;
+    } finally {
+      await this.close();
     }
   }
 
